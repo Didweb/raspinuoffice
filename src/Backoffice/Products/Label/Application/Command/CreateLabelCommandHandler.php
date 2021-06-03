5b1b522c8ce7d069990bddc00952a8a14c85fc76
Didweb <info@did-web.com>
2021-06-03 14:13:26 +0200

    Change position files

--- src/Backoffice/Products/Label/Application/Command/CreateLabelCommandHandler.php
+++ src/Backoffice/Products/Label/Application/Command/CreateLabelCommandHandler.php
@@ -2,9 +2,10 @@
 
 declare(strict_types=1);
 
-namespace RaspinuOffice\Backoffice\Products\Label\Application;
+namespace RaspinuOffice\Backoffice\Products\Label\Application\Command;
 
 
+use RaspinuOffice\Backoffice\Products\Label\Application\CreateLabelCommand;
 use RaspinuOffice\Backoffice\Products\Label\Application\Services\CreateLabel;
 use RaspinuOffice\Backoffice\Products\Label\Application\Services\CreateLabelServiceCommand;
 use RaspinuOffice\Backoffice\Products\Label\Domain\ValueObject\LabelId;
